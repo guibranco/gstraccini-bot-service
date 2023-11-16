58af96b995283abc743799ce5b460375aedcea89
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-11-16 00:33:52 +0000

    Feature/add skeleton (#3)
    * Add skeleton
    
    * Add PHP files
    
    * Deploy GitHub secrets

--- Src/comments.php
+++ Src/comments.php
@@ -1,3 +1,44 @@
 <?php
 
-echo "test";
+require_once("config.php");
+
+use Lcobucci\JWT\Configuration;
+use Lcobucci\JWT\Signer\Key\InMemory;
+use Lcobucci\JWT\Signer\Rsa\Sha256 as RSA;
+
+
+require 'vendor/autoload.php';
+
+
+function readComments()
+{
+    global $mySqlHost, $mySqlUser, $mySqlPassword, $mySqlDatabase;
+
+    $mysqli = new mysqli($mySqlHost, $mySqlUser, $mySqlPassword, $mySqlDatabase);
+    if ($mysqli->connect_errno) {
+        die("Failed to connect to MySQL: " . $mysqli->connect_error);
+    }
+
+    $mysqli->set_charset("utf8mb4");
+    $mysqli->query("SELECT * FROM github_comments WHERE Processed = 0");
+}
+
+function generateToken(){
+    global $gitHubAppId, $gitHubAppPrivateKey;
+
+    $private = openssl_pkey_get_private($gitHubAppPrivateKey);
+    $public = openssl_pkey_get_public($private);
+
+    $config = Configuration::forAsymmetricSigner(new RSA(), InMemory::plainText($private), InMemory::plainText($public));
+    $now   = new \DateTimeImmutable();
+
+    $token = $config->builder()
+    ->issuedBy($gitHubAppId)
+    ->issuedAt($now)
+    ->expiresAt($now->modify('+10 minutes'))
+    ->getToken($config->signer(), $config->signingKey());
+
+    echo $token;
+}
+
+generateToken();
