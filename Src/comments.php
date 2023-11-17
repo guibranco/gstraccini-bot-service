b6dd2a72e6ca066477a8e27f4cfdab051d4b71f5
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-11-17 00:02:15 +0000

    Process comments

--- Src/comments.php
+++ Src/comments.php
@@ -11,8 +11,9 @@ use Lcobucci\JWT\Token\Builder;
 
 require 'vendor/autoload.php';
 
-function readComments()
+function connectToDatabase()
 {
+
     global $mySqlHost, $mySqlUser, $mySqlPassword, $mySqlDatabase;
 
     $mysqli = new mysqli($mySqlHost, $mySqlUser, $mySqlPassword, $mySqlDatabase);
@@ -21,14 +22,48 @@ function readComments()
     }
 
     $mysqli->set_charset("utf8mb4");
-    $mysqli->query("SELECT * FROM github_comments WHERE Processed = 0");
+
+    return $mysqli;
 }
 
-function generateToken()
+function readComments()
+{
+    $mysqli = connectToDatabase();
+    $result = $mysqli->query("SELECT * FROM github_comments WHERE Processed = 0 LIMIT 10");
+
+    if (!$result) {
+        return null;
+    }
+
+    $comments = array();
+
+    while ($obj = $result->fetch_object()) {
+        $comments[] = $obj;
+    }
+
+    $result->close();
+    $mysqli->close();
+
+    return $comments;
+}
+
+function updateComment($commentSequence)
+{
+    $mysqli = connectToDatabase();
+    $sql = "UPDATE github_comments SET Processed = 1, ProcessedDate = NOW() WHERE Sequence = ?";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param("i", $commentSequence);
+
+    $stmt->execute();
+    $stmt->close();
+    $mysqli->close();
+}
+
+function generateAppToken()
 {
     global $gitHubAppId, $gitHubAppPrivateKey;
 
-    $tokenBuilder = (new Builder(new JoseEncoder(), ChainedFormatter::default()));
+    $tokenBuilder = new Builder(new JoseEncoder(), ChainedFormatter::default());
     $algorithm = new Sha256();
     $signingKey = InMemory::plainText($gitHubAppPrivateKey);
     $base = new \DateTimeImmutable();
@@ -37,48 +72,61 @@ function generateToken()
     $token = $tokenBuilder
         ->issuedBy($gitHubAppId)
         ->issuedAt($now->modify('-1 minute'))
-        ->expiresAt($now->modify('+10 minutes'))
+        ->expiresAt($now->modify('+5 minutes'))
         ->getToken($algorithm, $signingKey);
 
     return $token->toString();
 }
 
-function requestGitHub($url)
+function generateInstallationToken($installationId, $repositoryName)
 {
-    $gitHubToken = generateToken();
+    $gitHubAppToken = generateAppToken();
+
+    $data = new \stdClass();
+    $data->repository = $repositoryName;
+    $response = requestGitHub($gitHubAppToken, "app/installations/" . $installationId . "/access_tokens", $data);
+
+    $json = json_decode($response["body"]);
+    return $json->token;
+}
+
+function requestGitHub($gitHubToken, $url, $data = null)
+{
 
     $baseUrl = "https://api.github.com/";
 
     $headers = array();
     $headers[] = "User-Agent: " . USER_AGENT;
     $headers[] = "Content-type: application/json";
-    if (isset($gitHubToken)) {
-        $headers[] = "Authorization: Bearer " . $gitHubToken;
+    $headers[] = "Authorization: Bearer " . $gitHubToken;
+
+    $fields = array(
+        CURLOPT_URL => $baseUrl . $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HEADER => 1,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_SSL_VERIFYHOST => 0,
+        CURLOPT_SSL_VERIFYPEER => 0,
+        CURLOPT_HTTPHEADER => $headers
+    );
+
+    if ($data !== null) {
+        $fields[CURLOPT_POST] = true;
+        $fields[CURLOPT_POSTFIELDS] = json_encode($data);
     }
 
     $curl = curl_init();
 
-    curl_setopt_array(
-        $curl,
-        array(
-            CURLOPT_URL => $baseUrl . $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_HEADER => 1,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_SSL_VERIFYHOST => 0,
-            CURLOPT_SSL_VERIFYPEER => 0,
-            CURLOPT_HTTPHEADER => $headers
-        )
-    );
+    curl_setopt_array($curl, $fields);
 
     $response = curl_exec($curl);
 
     if ($response === false) {
-        echo $url;
+        echo htmlspecialchars($url);
         echo "\r\n";
         die(curl_error($curl));
     }
@@ -87,8 +135,59 @@ function requestGitHub($url)
     $header = substr($response, 0, $headerSize);
     $headers = getHeaders($header);
     $body = substr($response, $headerSize);
-    $http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
     curl_close($curl);
 
     return array("headers" => $headers, "body" => $body);
 }
+
+function handleComment($comment)
+{
+    $commentBody = strtolower($comment->CommentBody);
+    $token = generateInstallationToken($comment->InstallationId, $comment->RepositoryName);
+
+    $reactionUrl = "repos/" . $comment->RepositoryOwner . "/" . $comment->RepositoryName . "/issues/comments/" . $comment->CommentId . "/reactions";
+    $commentUrl = "repos/" . $comment->RepositoryOwner . "/" . $comment->RepositoryName . "/issues/" . $comment->IssueNumber . "/comments";
+
+    if ($comment->CommentUser !== "guibranco") {
+        requestGitHub($token, $reactionUrl, array("content" => "-1"));
+        return;
+    }
+
+    requestGitHub($token, $reactionUrl, array("content" => "eyes"));
+
+    if ($commentBody == "@gstraccini hello world") {
+        requestGitHub($token, $commentUrl, array("body" => "Hello @" . $comment->CommentUser . "!"));
+        requestGitHub($token, $reactionUrl, array("content" => "heart"));
+        return;
+    }
+
+    if ($commentBody == "@gstraccini thank you") {
+        requestGitHub($token, $commentUrl, array("body" => "You're welcome @" . $comment->CommentUser . "!"));
+        requestGitHub($token, $reactionUrl, array("content" => "+1"));
+        return;
+    }
+
+    if ($commentBody == "@gstraccini help"){
+        requestGitHub($token, $commentUrl, array("body"=> "That's what I can do: \r\n - @gstraccini hello world \r\n - @gstraccini thank you \r\n - @gstraccini help"));
+        requestGitHub($token, $reactionUrl, array("content"=> "rocker"));
+        return;
+    }
+    
+    requestGitHub($token, $commentUrl, array("body" => "I'm sorry @" . $comment->CommentUser . ", I can't do that."));
+    requestGitHub($token, $reactionUrl, array("content" => "+1"));
+    return;
+}
+
+
+function main()
+{
+    $comments = readComments();
+    foreach ($comments as $comment) {
+        handleComment($comment);
+        updateComment($comment->Sequence);
+    }
+}
+
+sendHealthCheck("/start");
+main();
+sendHealthCheck();
