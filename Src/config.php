520372bcaa41ec2c1067e106bc274cad21d51549
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-11-17 00:03:36 +0000

    Process comments (#8)

--- Src/config.php
+++ Src/config.php
@@ -1,11 +1,12 @@
 <?php
 
 ini_set("default_charset", "UTF-8");
-ini_set("date.timezone", "America/Sao_Paulo");
+if (!isset($_SERVER['REQUEST_METHOD'])) {
+    ini_set("date.timezone", "America/Sao_Paulo");
+}
 mb_internal_encoding("UTF-8");
 
 define("USER_AGENT", "gstraccini-bot (+https://github.com/apps/gstraccini-bot/)");
-
 
 $mySqlSecretsFile = "mySql.secrets.php";
 if (file_exists($mySqlSecretsFile)) {
@@ -15,4 +16,34 @@ if (file_exists($mySqlSecretsFile)) {
 $gitHubAppSecretsFile = "gitHubApp.secrets.php";
 if (file_exists($gitHubAppSecretsFile)) {
     require_once $gitHubAppSecretsFile;
+}
+
+$healthChecksIoSecretsFile = "healthChecksIo.secrets.php";
+if (file_exists($healthChecksIoSecretsFile)) {
+    require_once $healthChecksIoSecretsFile;
+}
+
+function sendHealthCheck($type=null)
+{
+    if (isset($_SERVER['REQUEST_METHOD'])) {
+        return;
+    }
+    global $healthChecksIoUuid;
+
+    $curl = curl_init();
+    curl_setopt_array(
+        $curl,
+        array(
+            CURLOPT_URL => "https://hc-ping.com/" . $healthChecksIoUuid . ($type == null ? "" : $type),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_SSL_VERIFYPEER => false
+        )
+    );
+    curl_exec($curl);
+    curl_close($curl);
 }
