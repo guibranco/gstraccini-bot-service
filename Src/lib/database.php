f3691f9d35477a33344bbebc51823f86bf8e8bfb
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-11-23 12:47:07 +0000

    Add review command processing

--- Src/lib/database.php
+++ Src/lib/database.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 function connectToDatabase()
 {
@@ -14,11 +14,12 @@ function connectToDatabase()
     return $mysqli;
 }
 
-function readTable($tableName, $where = null){
+function readTable($tableName, $where = null)
+{
     $mysqli = connectToDatabase();
     $defaultWhere = "Processed = 0 ORDER BY Sequence ASC LIMIT 10";
     $sql = "SELECT * FROM " . $tableName . " WHERE ";
-    if($where == null){
+    if ($where == null) {
         $sql .= $defaultWhere;
     } else {
         $sql .= $where;
@@ -29,25 +30,68 @@ function readTable($tableName, $where = null){
         return null;
     }
 
-    $data = array();
-
-    while ($obj = $result->fetch_object()) {
-        $data[] = $obj;
-    }
-
+    $data = $result->fetch_all(MYSQLI_ASSOC);
     $result->close();
     $mysqli->close();
 
     return $data;
 }
 
-function updateTable($tableName, $sequence) {
+function updateTable($tableName, $sequence)
+{
     $mysqli = connectToDatabase();
     $sql = "UPDATE " . $tableName . " SET Processed = 1, ProcessedDate = NOW() WHERE Sequence = ?";
     $stmt = $mysqli->prepare($sql);
     $stmt->bind_param("i", $sequence);
 
     $stmt->execute();
+    $stmt->close();
+    $mysqli->close();
+}
+
+function upsertPullRequest($pullRequest)
+{
+    $mysqli = connectToDatabase();
+    $sql = "SELECT Sequence FROM github_pull_requests WHERE NodeId = ?";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param("s", $nodeId);
+
+    $nodeId = $pullRequest->NodeId;
+
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+
+    $stmt->close();
+
+    if ($row) {
+        $sql = "UPDATE github_pull_requests SET Processed = 0, ProcessedDate = NULL WHERE Sequence = ?";
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param("i", $sequence);
+        $sequence = $row;
+        $stmt->execute();
+    } else {
+        $sql = "INSERT INTO github_pull_requests (GitHubHookId, GitHubHookInstallationTargetId, RepositoryOwner, RepositoryName, PullRequestId, PullRequestNumber, PullRequestSubmitter, NodeId, Title, Ref, InstallationId) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param('iissiissssi', $githubId, $gitHubInstallationId, $repositoryOwner, $repositoryName, $pullRequestId, $pullRequestNumber, $pullRequestSubmitter, $nodeId, $title, $ref, $installationId);
+
+        $githubId = $pullRequest->HookId;
+        $gitHubInstallationId = $pullRequest->HookInstallationTargetId;
+        $repositoryOwner = $pullRequest->RepositoryOwner;
+        $repositoryName = $pullRequest->RepositoryName;
+        $pullRequestId = $pullRequest->Id;
+        $pullRequestNumber = $pullRequest->Number;
+        $pullRequestSubmitter = $pullRequest->Submitter;
+        $nodeId = $pullRequest->NodeId;
+        $title = $pullRequest->Title;
+        $ref = $pullRequest->Ref;
+        $installationId = $pullRequest->InstallationId;
+
+        if (!$stmt->execute()) {
+            die("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
+        }
+    }
+
     $stmt->close();
     $mysqli->close();
 }
