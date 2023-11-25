72188dca0afe525fcc310e6bc8e820ee07eb2d1c
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-11-25 20:42:46 +0000

    Fix build pipeline

--- Tools/db-check.sh
+++ Tools/db-check.sh
@@ -21,14 +21,12 @@ fi
 
 PullRequets= $(mysql -h "$MYSQL_HOST" --protocol tcp "--user=$MYSQL_USER" "--database=$MYSQL_DB" -sse \
     "SELECT COUNT(*) FROM information_schema.tables WHERE \
-     table_schema='$MYSQL_DB' AND table_name='pull_requests';")
+     table_schema='$MYSQL_DB' AND table_name='github_pull_requests';")
 Comments=$(mysql -h "$MYSQL_HOST" --protocol tcp "--user=$MYSQL_USER" "--database=$MYSQL_DB" -sse \
     "SELECT COUNT(*) FROM information_schema.tables WHERE \
-     table_schema='$MYSQL_DB' AND table_name='pull_requests_comments';")
+     table_schema='$MYSQL_DB' AND table_name='github_pull_requests_comments';")
 
 if [[ $PullRequets -eq 0 && $Comments -eq 0 ]]; then
     echo "::error file=$0,line=$LINENO::The github_pull_requests and github_pull_requests_comments tables does not exists."
-    mysql -h "$MYSQL_HOST" --protocol tcp "--user=$MYSQL_USER" "--database=$MYSQL_DB" -e \
-        "SELECT * FROM information_schema.tables;"
     exit 1
 fi
