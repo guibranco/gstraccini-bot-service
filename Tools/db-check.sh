e71b7f6110f1c34a8e17756fd6553ad2a1069228
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-11-25 20:39:18 +0000

    Show tables

--- Tools/db-check.sh
+++ Tools/db-check.sh
@@ -29,5 +29,7 @@ Comments=$(mysql -h "$MYSQL_HOST" --protocol tcp "--user=$MYSQL_USER" "--databas
 if [[ $PullRequets -eq 0 && $Comments -eq 0 ]]; then
     echo "::error file=$0,line=$LINENO::The github_pull_requests and github_pull_requests_comments tables does not exists."
     mysql -h "$MYSQL_HOST" --protocol tcp "--user=$MYSQL_USER" "--database=$MYSQL_DB" -e "SHOW TABLES;"
+    mysql -h "$MYSQL_HOST" --protocol tcp "--user=$MYSQL_USER" "--database=$MYSQL_DB" -e \
+        "SELECT COUNT(*) FROM information_schema.tables;"
     exit 1
 fi
