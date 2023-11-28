3be38e41e1169b465ed5df05e2fbd53e5457c12e
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2023-11-28 23:19:18 +0000

    Fix typo

--- Src/config/config.php
+++ Src/config/config.php
@@ -41,5 +41,5 @@ function loadConfig()
 
 require_once "lib/functions.php";
 require_once "lib/database.php";
-require_once "lib/appVeyor.php";
+require_once "lib/appveyor.php";
 require_once "lib/github.php";
