d78006433e5f3c0c4ad53bd926197f0137ef667c
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-01-30 23:32:01 +0000

    Update migration scripts

--- Tools/db-check.sh
+++ Tools/db-check.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ $# -ne 3 ]; then
     echo "::error file=$0,line=$LINENO::Usage: $0 <host> <user> <database>"
     exit 1
@@ -7,24 +9,40 @@ MYSQL_HOST="$1"
 MYSQL_USER="$2"
 MYSQL_DB="$3"
 
+DB_ENV="PRD"
+
+if [[ "$MYSQL_HOST" == "localhost" && "$MYSQL_USER" == "test" ]]; then
+    DB_ENV="CI [Docker]"
+fi
+
 CONNECT=$(
     mysql -h "$MYSQL_HOST" --protocol tcp "--user=$MYSQL_USER" --batch --skip-column-names -e \
-        "SHOW DATABASES LIKE '"$DBNAME"';" | grep "$DBNAME" >/dev/null
+        "SHOW DATABASES LIKE '$DBNAME';" | grep "$DBNAME" >/dev/null
     echo "$?"
 )
 
-if [ $CONNECT -eq 1 ]; then
+if [ "$CONNECT" -eq 1 ]; then
     MESSAGE=""
     echo "::error file=$0,line=$LINENO::The database does not exist or cannot be accessed using these credentials."
     exit 1
 fi
 
-PullRequets= $(mysql -h "$MYSQL_HOST" --protocol tcp "--user=$MYSQL_USER" "--database=$MYSQL_DB" -sse \
-    "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema='$MYSQL_DB' AND table_name='github_pull_requests';")
-Comments=$(mysql -h "$MYSQL_HOST" --protocol tcp "--user=$MYSQL_USER" "--database=$MYSQL_DB" -sse \
-    "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema='$MYSQL_DB' AND table_name='github_pull_requests_comments';")
+EXISTS=$(mysql -h "$MYSQL_HOST" --protocol tcp "--user=$MYSQL_USER" "--database=$MYSQL_DB" -sse \
+    "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema='$MYSQL_DB' AND table_name='schema_version';")
 
-if [[ $PullRequets -eq 0 && $Comments -eq 0 ]]; then
-    echo "::error file=$0,line=$LINENO::The github_pull_requests and github_pull_requests_comments tables does not exists."
-    exit 1
+echo "### Database check summary (\`$DB_ENV\` environment) :eyes:" >>"$GITHUB_STEP_SUMMARY"
+echo "" >>"$GITHUB_STEP_SUMMARY"
+
+if [ "$EXISTS" -eq 1 ]; then
+    MESSAGE="The \`schema version\` table **does** exist."
+    echo "::notice file=$0,line=$LINENO::$MESSAGE"
+    echo ":thumbsup: $MESSAGE" >>"$GITHUB_STEP_SUMMARY"
+    echo "db_check_not_found=false" >>"$GITHUB_ENV"
+    echo "not_found=false" >>"$GITHUB_OUTPUT"
+else
+    MESSAGE="The \`schema version\` table **does not** exist."
+    echo "::warning file=$0,line=$LINENO::$MESSAGE"
+    echo ":thumbsdown: $MESSAGE" >>"$GITHUB_STEP_SUMMARY"
+    echo "db_check_not_found=true" >>"$GITHUB_ENV"
+    echo "not_found=true" >>"$GITHUB_OUTPUT"
 fi
