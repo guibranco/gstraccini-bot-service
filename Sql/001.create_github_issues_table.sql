e74549f2ad748e0cd40d5b78bbcdd34c0736fdf2
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-02-07 23:52:49 +0000

    Create databases

---
+++ Sql/001.create_github_issues_table.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS `github_issues`;
+
+CREATE TABLE
+    `github_issues` (
+        `Sequence` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+        `Date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        `DeliveryId` BINARY(16) NOT NULL,
+        `DeliveryIdText` VARCHAR(36) GENERATED ALWAYS AS (
+            INSERT(INSERT(INSERT(INSERT(hex(`DeliveryId`),9,0,'-'),14,0,'-'),19,0,'-'),24,0,'-')
+        ) VIRTUAL,
+        `HookId` BIGINT UNSIGNED NOT NULL,
+        `TargetId` BIGINT UNSIGNED NOT NULL,
+        `TargetType` VARCHAR(255) NOT NULL,        
+        `RepositoryOwner` VARCHAR(250) NOT NULL,
+        `RepositoryName` VARCHAR(250) NOT NULL,
+        `Id` BIGINT UNSIGNED NOT NULL,
+        `Number` BIGINT UNSIGNED NOT NULL,
+        `NodeId` VARCHAR(250) NOT NULL,
+        `Title` TEXT NOT NULL,
+        `Sender` VARCHAR(250) NOT NULL,
+        `InstallationId` BIGINT UNSIGNED NULL,
+        `Processed` BOOLEAN NOT NULL DEFAULT FALSE,
+        `ProcessedDate` TIMESTAMP NULL,
+        PRIMARY KEY (`Sequence`)
+    ) ENGINE = InnoDB;
+
+ALTER TABLE `github_issues` CONVERT TO CHARACTER SET utf8;
---
+++ Sql/004.create_github_signature_table.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS `github_signature`;
+
+CREATE TABLE
+    `github_signature` (
+        `Sequence` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+        `Date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        `DeliveryId` BINARY(16) NOT NULL,
+        `DeliveryIdText` VARCHAR(36) GENERATED ALWAYS AS (
+            INSERT(INSERT(INSERT(INSERT(hex(`DeliveryId`),9,0,'-'),14,0,'-'),19,0,'-'),24,0,'-')
+        ) VIRTUAL,
+        `HookId` BIGINT UNSIGNED NOT NULL,
+        `TargetId` BIGINT UNSIGNED NOT NULL,
+        `TargetType` VARCHAR(255) NOT NULL,        
+        `RepositoryOwner` VARCHAR(250) NOT NULL,
+        `RepositoryName` VARCHAR(250) NOT NULL,
+        `Event` VARCHAR(250) NOT NULL,
+        `ExpectedHash` VARCHAR(250) NULL,
+        `ActualHash` VARCHAR(250) NULL,
+        `InstallationId` BIGINT UNSIGNED NULL,
+        `Error` VARCHAR(250) NOT NULL,
+        `ResponseHeaders` TEXT NULL,
+        `ResponsePayload` TEXT NULL,
+        `Processed` BOOLEAN NOT NULL DEFAULT FALSE,
+        `ProcessedDate` TIMESTAMP NULL,
+        PRIMARY KEY (`Sequence`)
+    ) ENGINE = InnoDB;
+
+ALTER TABLE `github_signature` CONVERT TO CHARACTER SET utf8;
