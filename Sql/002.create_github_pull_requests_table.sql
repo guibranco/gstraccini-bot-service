9ed3e3b0a78698e56dbbae89b7e64a6c9bf4d96d
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-02-07 23:55:18 +0000

    Create databases (#130)

---
+++ Sql/002.create_github_pull_requests_table.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS `github_pull_requests`;
+
+CREATE TABLE
+    `github_pull_requests` (
+        `Sequence` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+        `Date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        `DeliveryId` BINARY(16) NOT NULL,
+        `DeliveryIdText` VARCHAR(36) GENERATED ALWAYS AS (
+            INSERT(INSERT(INSERT(INSERT(hex(`DeliveryId`),9,0,'-'),14,0,'-'),19,0,'-'),24,0,'-')
+        ) VIRTUAL,
+        `HookId` BIGINT UNSIGNED NOT NULL,
+        `TargetId` BIGINT UNSIGNED NOT NULL,
+        `TargetType` VARCHAR(255) NOT NULL,        
+        `RepositoryOwner` VARCHAR(250) NOT NULL,
+        `RepositoryName` VARCHAR(250) NOT NULL,
+        `Id` BIGINT UNSIGNED NOT NULL,
+        `Number` BIGINT UNSIGNED NOT NULL,
+        `Sender` VARCHAR(250) NOT NULL,
+        `NodeId` VARCHAR(250) NOT NULL,
+        `Title` TEXT NOT NULL,
+        `Ref` VARCHAR(250) NOT NULL,
+        `InstallationId` BIGINT UNSIGNED NULL,
+        `Processed` BOOLEAN NOT NULL DEFAULT FALSE,
+        `ProcessedDate` TIMESTAMP NULL,
+        PRIMARY KEY (`Sequence`)
+    ) ENGINE = InnoDB;
+
+ALTER TABLE `github_pull_requests` CONVERT TO CHARACTER SET utf8;
