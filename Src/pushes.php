bb3290d8c2173081bdd43d8d299a2fefc6b731fc
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-04-20 15:53:16 +0100

    Add push worker (#249)
    * Add push worker
    
    * Remove duplication
    
    * style: format code with PHP CS Fixer
    
    This commit fixes the style issues introduced in b5d4643 according to the output
    from PHP CS Fixer.
    
    Details: https://github.com/guibranco/gstraccini-bot/pull/249
    
    ---------
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

---
+++ Src/pushes.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once "config/config.php";
+
+use GuiBranco\GStracciniBot\Library\HealthChecks;
+
+function handlePush($push)
+{
+    $token = generateInstallationToken($push->InstallationId, $push->RepositoryName);
+
+    $botDashboardUrl = "https://bot.straccini.com/dashboard";
+    $commitQueryString =
+        "?owner=" . $push->RepositoryOwner .
+        "&repo=" . $push->RepositoryName .
+        "&ref=" . urlencode($push->Ref);
+
+    $repoPrefix = "repos/" . $push->RepositoryOwner . "/" . $push->RepositoryName;
+    $metadata = array(
+        "token" => $token,
+        "repoUrl" => $repoPrefix,
+        "checkRunUrl" => $repoPrefix . "/check-runs",
+        "dashboardUrl" => $botDashboardUrl . $commitQueryString
+    );
+
+    $checkRunId = setCheckRunInProgress($metadata, $push->HeadCommitId, "commit");
+    setCheckRunCompleted($metadata, $checkRunId, "commit");
+}
+
+function main()
+{
+    $pushes = readTable("github_pushes");
+    foreach ($pushes as $push) {
+        handlePush($push);
+        updateTable("github_pushes", $push->Sequence);
+    }
+}
+
+$healthCheck = new HealthChecks($healthChecksIoPushes);
+$healthCheck->start();
+main();
+$healthCheck->end();
