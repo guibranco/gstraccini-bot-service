340682e3aa51326ea20c772d7aca5902333af2bd
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-05-29 23:38:57 +0100

    Update pullRequests.php

--- Src/pullRequests.php
+++ Src/pullRequests.php
@@ -2,6 +2,7 @@
 
 require_once "config/config.php";
 
+use GuiBranco\Pancake\GUIDv4;
 use GuiBranco\GStracciniBot\Library\HealthChecks;
 
 define("ISSUES", "/issues/");
@@ -252,12 +253,13 @@ function main()
     }
 }
 
-$healthCheck = new HealthChecks($healthChecksIoPullRequests);
+$healthCheck = new HealthChecks($healthChecksIoPullRequests, GUIDv4::random());
 $healthCheck->start();
 $time = time();
 while (true) {
     main();
-    if ($time + 60 < time()) {
+    $limit = $time + 60;
+    if ($limit < time()) {
         break;
     }
 }
