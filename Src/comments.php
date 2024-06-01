ca1c143e3bb9203ef3f6122a73f5df24b1763aa1
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-06-01 02:13:59 +0100

    Update comments.php

--- Src/comments.php
+++ Src/comments.php
@@ -177,7 +177,7 @@ function execute_appveyorBumpVersion($config, $metadata, $comment)
 
     if (count($matches) === 2 && $matches[1] === "build") {
         doRequestGitHub($metadata["token"], $metadata["reactionUrl"], array("content" => "rocket"), "POST");
-        updateNextBuildNumber($metadata, $project, $project->nextBuildNumber + 1);
+        updateNextBuildNumber($metadata, $project, $project->buildNumber + 1);
     } elseif (count($matches) === 2 && ($matches[1] === "minor" || $matches[1] === "major")) {
         doRequestGitHub($metadata["token"], $metadata["reactionUrl"], array("content" => "-1"), "POST");
         $body = $metadata["errorMessages"]["notImplemented"];
