b2a41895f4f8a47b9545676a61d98dce860082d3
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-08-17 19:37:47 +0100

    Update pullRequests.php (#406)
    * Update pullRequests.php
    
    * Update pullRequests.php

--- Src/pullRequests.php
+++ Src/pullRequests.php
@@ -155,7 +155,7 @@ function checkForOtherPullRequests($metadata, $pullRequest)
             $prUpsert->Ref = $pullRequestPending->head->ref;
             $prUpsert->InstallationId = $pullRequest->InstallationId;
             upsertPullRequest($prUpsert);
-            echo "Triggering review of #{$pullRequestPending->number} - Sender: " . $pullRequest->Sender . " ✅ {$pullRequestPending->html_url}\n";
+            echo "State: {$pullRequestPending->mergeable_state} - Triggering review of #{$pullRequestPending->number} - Sender: " . $pullRequest->Sender . " ✅\n";
             break;
         }
     }
@@ -306,10 +306,10 @@ function resolveConflicts($metadata, $pullRequest, $pullRequestUpdated)
 {
     if ($pullRequestUpdated->mergeable_state != "clean" && !$pullRequestUpdated->mergeable) {
         if ($pullRequest->Sender !== "dependabot[bot]" && $pullRequest->Sender !== "depfu[bot]") {
-            echo "State: " . $pullRequestUpdated->mergeable_state . " Resolve Conflicts - Is NOT mergeable - Sender: " . $pullRequest->Sender . " ⛔\n";
+            echo "State: " . $pullRequestUpdated->mergeable_state . " - Resolve Conflicts - Is NOT mergeable - Sender: " . $pullRequest->Sender . " ⛔\n";
             return;
         }
-        echo "State: " . $pullRequestUpdated->mergeable_state . " Resolve Conflicts - Recreate - Sender: " . $pullRequest->Sender . " ☢️\n";
+        echo "State: " . $pullRequestUpdated->mergeable_state . " - Resolve Conflicts - Recreate - Sender: " . $pullRequest->Sender . " ☢️\n";
 
         if ($pullRequest->Sender !== "dependabot[bot]") {
             $comment = array("body" => "@dependabot recreate");
@@ -319,7 +319,7 @@ function resolveConflicts($metadata, $pullRequest, $pullRequestUpdated)
 
         doRequestGitHub($metadata["userToken"], $metadata["commentsUrl"], $comment, "POST");
     } else {
-        echo "State: " . $pullRequestUpdated->mergeable_state . " Resolve Conflicts - No conflicts - Sender: " . $pullRequest->Sender . " ⚠️\n";
+        echo "State: " . $pullRequestUpdated->mergeable_state . " - Resolve Conflicts - No conflicts - Sender: " . $pullRequest->Sender . " ⚠️\n";
     }
 }
 
