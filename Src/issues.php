64780ee78728c0f32aaeafd7c1aaa846595ccb6e
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-08-29 11:58:06 +0100

    Refactor label removal in issues.php for better functionality (#469)
    * Update issues.php
    
    * Update issues.php
    
    * Update issues.php
    
    * Update issues.php
    
    * style: format code with PHP CS Fixer
    
    This commit fixes the style issues introduced in fd7abf7 according to the output
    from PHP CS Fixer.
    
    Details: https://github.com/guibranco/gstraccini-bot/pull/469
    
    ---------
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- Src/issues.php
+++ Src/issues.php
@@ -22,7 +22,7 @@ function handleIssue($issue)
     $issueUpdated = json_decode($issueResponse->body);
 
     if ($issueUpdated->assignee != null) {
-        removeAwaitingTriageLabel($issueUpdated, $metadata);
+        removeLabels($issueUpdated, $metadata, true);
         return;
     }
 
@@ -42,7 +42,7 @@ function handleIssue($issue)
     addLabels($issueUpdated, $collaboratorsLogins, $metadata);
 
     if(in_array($issueUpdated->user->login, $collaboratorsLogins)) {
-        removeAwaitingTriageLabel($issueUpdated, $metadata);
+        removeLabels($issueUpdated, $metadata);
     }
 }
 
@@ -63,12 +63,21 @@ function addLabels($issueUpdated, $collaboratorsLogins, $metadata)
     }
 }
 
-function removeAwaitingTriageLabel($issueUpdated, $metadata)
+function removeLabels($issueUpdated, $metadata, $includeWip = false)
 {
-    $awaitingTriageLabel = "🚦awaiting triage";
+    $labelsLookup = [
+        "🚦awaiting triage",
+        "⏳ awaiting response"
+    ];
+    if ($includeWip === true) {
+        $labelsLookup[] = "🛠 WIP";
+    }
+
     $labels = array_column($issueUpdated->labels, "name");
-    if (in_array($awaitingTriageLabel, $labels)) {
-        $url = "{$metadata["issuesUrl"]}/{$issueUpdated->number}/labels/{$awaitingTriageLabel}";
+    $intersect = array_intersect($labelsLookup, $labels);
+
+    foreach ($intersect as $label) {
+        $url = "{$metadata["issuesUrl"]}/{$issueUpdated->number}/labels/{$label}";
         doRequestGitHub($metadata["token"], $url, null, "DELETE");
     }
 }
