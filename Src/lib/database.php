256370d4d02c5e832f3d65df41e8e14607b68091
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-09-01 04:07:01 +0100

    Enhance MySQL connection handling in database.php (#492)
    * Update database.php
    
    * style: format code with PHP CS Fixer
    
    This commit fixes the style issues introduced in ed96901 according to the output
    from PHP CS Fixer.
    
    Details: https://github.com/guibranco/gstraccini-bot/pull/492
    
    * Update database.php
    
    ---------
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- Src/lib/database.php
+++ Src/lib/database.php
@@ -8,7 +8,10 @@ function connectToDatabase($isRetry = false)
 
     try {
         $mysqli = new mysqli($mySqlHost, $mySqlUser, $mySqlPassword, $mySqlDatabase);
-        if ($mysqli->connect_errno) {
+        if ($mysqli->connect_errno && $isRetry === false) {
+            sleep(10);
+            return connectToDatabase(true);
+        } elseif ($mysqli->connect_errno) {
             die("Failed to connect to MySQL: " . $mysqli->connect_error);
         }
 
