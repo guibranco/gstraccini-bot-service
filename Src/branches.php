57b91151a2435d57aee723b55b0ed3e80a65d50b
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-09-09 00:55:08 +0100

    Update branches.php

--- Src/branches.php
+++ Src/branches.php
@@ -5,7 +5,7 @@ require_once "config/config.php";
 use GuiBranco\Pancake\GUIDv4;
 use GuiBranco\Pancake\HealthChecks;
 
-function handleBranch($branch)
+function handleItem($branch)
 {
     $token = generateInstallationToken($branch->InstallationId, $branch->RepositoryName);
 
@@ -146,12 +146,16 @@ function getReferencedIssueByBranch($metadata, $branch)
 
 function main()
 {
-    $config = loadConfig();
+   $config = loadConfig();
     ob_start();
-    $branches = readTable("github_branches");
-    foreach ($branches as $branch) {
-        updateTable("github_branches", $branch->Sequence);
-        handleBranch($branch);        
+    $table = "github_branches";
+    $items = readTable($table);
+    foreach ($items as $item) {
+        echo "Sequence: {$item->Sequence}\n";
+        echo "Delivery ID: {$item->DeliveryIdText}\n";
+        updateTable($table, $item->Sequence);
+        handleItem($item);        
+        echo str_repeat("=-", 50) . "=\n";
     }
     $result = ob_get_clean();
     if ($config->debug->all === true || $config->debug->branches === true) {
