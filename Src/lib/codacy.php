bfed626413c1cf7ca645bd341dd2197800ca8cf4
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-11-19 23:46:05 +0000

    Feature/697 feature add command to reanalyze commit in codacy (#698)
    * #697 feat - Add function to reanalyze a commit in Codacy.
    
    * #697 feat - Add command to reanalyze last commit in Codacy.
    
    * #697 feat - Add command for reanalyzing commit in Codacy.
    
    * #697 feat - Add commands for Codacy reanalysis in README.
    
    * #697 feat - Add command to reanalyze commit in Codacy.
    
    * #697 feat - Adds command to reanalyze commit in Codacy.

--- Src/lib/codacy.php
+++ Src/lib/codacy.php
@@ -41,7 +41,51 @@ function bypassPullRequestAnalysis(string $remoteOrganizationName, string $repos
 
     if ($response->statusCode >= 300) {
         $info = json_encode(array("url" => $url, "response" => $response));
-        $logger->log("Error on Codacy request", $info);
+        $logger->log("Error on Codacy request (bypass)", $info);
+    }
+
+    return $response;
+}
+
+
+/**
+ * The function reanalyzeCommit sends a POST request to the Codacy API to reanalyze a specific commit
+ * in a repository.
+ *
+ * @param string remoteOrganizationName The `remoteOrganizationName` parameter represents the name of
+ * the organization on the remote repository hosting service (e.g., GitHub) where the repository is
+ * located. This could be the GitHub organization name if the repository is hosted on GitHub.
+ * @param string repositoryName The `repositoryName` parameter in the `reanalyzeCommit` function refers
+ * to the name of the repository for which you want to reanalyze a specific commit on Codacy. This
+ * parameter should be a string that represents the name of the repository on Codacy.
+ * @param string commitUUID The `commitUUID` parameter represents the unique identifier (UUID) of the
+ * commit that you want Codacy to reanalyze. This UUID uniquely identifies the commit in the
+ * repository.
+ *
+ * @return stdClass The function `reanalyzeCommit` returns an object of type `stdClass`, which is the
+ * response from the Codacy API after attempting to reanalyze the specified commit in a repository for
+ * a given organization.
+ */
+function reanalyzeCommit(string $remoteOrganizationName, string $repositoryName, string $commitUUID): stdClass
+{
+    global $codacyApiToken, $logger;
+
+    $baseUrl = "https://api.codacy.com/api/v3/";
+    $url = "organizations/gh/{$remoteOrganizationName}/repositories/{$repositoryName}/reanalyzeCommit";
+
+    $headers = array(
+        constant("USER_AGENT"),
+        "api-token: " . $codacyApiToken,
+        "Accept: application/json"
+    );
+
+    $request = new Request();
+
+    $response = $request->post($baseUrl . $url, $headers, array("commit" => $commitUUID, "cleanCache" => false));
+
+    if ($response->statusCode >= 300) {
+        $info = json_encode(array("url" => $url, "response" => $response));
+        $logger->log("Error on Codacy request (reanalyze)", $info);
     }
 
     return $response;
