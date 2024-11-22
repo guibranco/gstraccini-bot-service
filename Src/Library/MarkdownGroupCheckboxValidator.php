000ea07e08bbf0314430a891cd6e4daf575ca24d
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-11-22 19:01:59 +0000

    Update MarkdownGroupCheckboxValidator.php

--- Src/Library/MarkdownGroupCheckboxValidator.php
+++ Src/Library/MarkdownGroupCheckboxValidator.php
@@ -6,18 +6,23 @@ class MarkdownGroupCheckboxValidator
 {
     public function validateCheckboxes(string $prBody): array
     {
-        $prBody = str_replace("\r", '', $prBody);
+        $prBody = str_replace("\r", "", $prBody);
         $groupPattern = '/##\s(.+)\n(?:\<!--.*?--\>\n)?((?:- \[(.)\] .+\n)+)/i';
-        $checkboxPattern = '/- \[(x| )\] (.+)/i';
+        $checkboxPattern = "/- \[(x| )\] (.+)/i";
 
         $report = [
-            'found' => 0,
-            'groups' => [],
-            'errors' => [],
+            "found" => 0,
+            "groups" => [],
+            "errors" => [],
         ];
 
-        $found = preg_match_all($groupPattern, $prBody, $groupMatches, PREG_SET_ORDER);
-        $report['found'] = $found;
+        $found = preg_match_all(
+            $groupPattern,
+            $prBody,
+            $groupMatches,
+            PREG_SET_ORDER
+        );
+        $report["found"] = $found;
 
         if (!$found) {
             return $report;
@@ -26,30 +31,37 @@ class MarkdownGroupCheckboxValidator
         foreach ($groupMatches as $groupMatch) {
             $groupTitle = trim($groupMatch[1]);
 
-            preg_match_all($checkboxPattern, $groupMatch[0], $checkboxMatches, PREG_SET_ORDER);
+            preg_match_all(
+                $checkboxPattern,
+                $groupMatch[0],
+                $checkboxMatches,
+                PREG_SET_ORDER
+            );
 
             $groupResult = [
-                'group' => $groupTitle,
-                'checked' => [],
-                'unchecked' => [],
+                "group" => $groupTitle,
+                "checked" => [],
+                "unchecked" => [],
             ];
 
             $hasChecked = false;
             foreach ($checkboxMatches as $checkboxMatch) {
                 $checkboxText = trim($checkboxMatch[2]);
-                if (strtolower($checkboxMatch[1]) === 'x') {
-                    $groupResult['checked'][] = $checkboxText;
+                if (strtolower($checkboxMatch[1]) === "x") {
+                    $groupResult["checked"][] = $checkboxText;
                     $hasChecked = true;
                 } else {
-                    $groupResult['unchecked'][] = $checkboxText;
+                    $groupResult["unchecked"][] = $checkboxText;
                 }
             }
 
             if (!$hasChecked) {
-                $report['errors'][] = "No checkbox selected in group: $groupTitle";
+                $report[
+                    "errors"
+                ][] = "No checkbox selected in group: $groupTitle";
             }
 
-            $report['groups'][] = $groupResult;
+            $report["groups"][] = $groupResult;
         }
 
         return $report;
@@ -57,27 +69,30 @@ class MarkdownGroupCheckboxValidator
 
     public function generateReport(array $validationResult): string
     {
-        if (isset($validationResult['errors']) && !empty($validationResult['errors'])) {
-            return implode("\n", $validationResult['errors']);
+        if (
+            isset($validationResult["errors"]) &&
+            !empty($validationResult["errors"])
+        ) {
+            return implode("\n", $validationResult["errors"]);
         }
 
         $report = "Checkbox validation report:\n";
 
-        foreach ($validationResult['groups'] as $group) {
-            $report .= "\n{$group['group']}\n";
+        foreach ($validationResult["groups"] as $group) {
+            $report .= "\n{$group["group"]}\n";
 
-            if (!empty($group['checked'])) {
+            if (!empty($group["checked"])) {
                 $report .= "Checked items:\n";
-                foreach ($group['checked'] as $checkedItem) {
+                foreach ($group["checked"] as $checkedItem) {
                     $report .= "- $checkedItem\n";
                 }
             } else {
                 $report .= "No checked items.\n";
             }
 
-            if (!empty($group['unchecked'])) {
+            if (!empty($group["unchecked"])) {
                 $report .= "Unchecked items:\n";
-                foreach ($group['unchecked'] as $uncheckedItem) {
+                foreach ($group["unchecked"] as $uncheckedItem) {
                     $report .= "- $uncheckedItem\n";
                 }
             }
