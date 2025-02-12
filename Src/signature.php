347faabfcbb26b8a9dce22a9fa98ad3c5b4d6541
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-02-12 11:46:22 +0000

    #310 remove - revert changes made in signature.php file

--- Src/signature.php
+++ Src/signature.php
@@ -1,33 +1,19 @@
 <?php
-use Src\LabelHandler;
 
 require_once "config/config.php";
 
 use GuiBranco\GStracciniBot\Library\ProcessingManager;
 use GuiBranco\Pancake\GUIDv4;
-    global $gitHubUserToken, $gitHubWebhookEndpoint, $gitHubWebhookSignature;
 use GuiBranco\Pancake\HealthChecks;
 
 function handleItem($signature)
 {
     global $gitHubUserToken, $gitHubWebhookEndpoint, $gitHubWebhookSignature;
-    $githubClient = new GitHubClient($gitHubUserToken);
-    $labelHandler = new LabelHandler($githubClient);
-
-    // Example payload processing
-    $payload = json_decode($signature, true);
 
     $request = array(
         "content_type" => "json",
         "insecure_ssl" => "0",
         "url" => $gitHubWebhookEndpoint,
-);
-
-if (isset($payload['comment']['body']) && isset($payload['issue']['number'])) {
-    $commentBody = $payload['comment']['body'];
-    $issueOrPrNumber = $payload['issue']['number'];
-    $repository = $payload['repository']['full_name'];
-        "secret" => $gitHubWebhookSignature
     );
 
     $repoPrefix = "repos/" . $signature->RepositoryOwner . "/" . $signature->RepositoryName;
