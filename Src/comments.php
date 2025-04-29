76a93c4aff5e7eef2308ea4e4295ffeeaa819496
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-04-29 04:35:30 +0100

    Update comments.php

--- Src/comments.php
+++ Src/comments.php
@@ -67,7 +67,7 @@ function handleItem($comment): void
         "commentUrl" => $repoPrefix . "/issues/" . $comment->PullRequestNumber . "/comments",
         "labelsUrl" => $repoPrefix . "/labels",
         "checkRunUrl" => $repoPrefix . "/check-runs",
-        "dashboardUrl" => $config->dashboardUrl . $prQueryString
+        "dashboardUrl" => $config->dashboardUrl . $prQueryString,
         "errorMessages" => array(
             "notCollaborator" => $prefix . $suffix . " You aren't a collaborator in this repository." . $emoji,
             "invalidParameter" => $prefix . $suffix . " Invalid parameter." . $emoji,
