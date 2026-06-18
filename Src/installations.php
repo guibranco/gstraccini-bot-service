e11ed8e2f809ed85da920120e8491e64e72e3ab9
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2026-06-18 16:45:33 +0100

    Add installations.php for handling user items

---
+++ Src/installations.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once "config/config.php";
+
+use GuiBranco\GStracciniBot\Library\LabelHelper;
+use GuiBranco\GStracciniBot\Library\LabelService;
+use GuiBranco\GStracciniBot\Library\ProcessingManager;
+use GuiBranco\GStracciniBot\Library\RepositoryManager;
+use GuiBranco\Pancake\GUIDv4;
+use GuiBranco\Pancake\HealthChecks;
+
+function handleItem($user){ }
+
+$healthCheck = new HealthChecks($healthChecksIoInstallations, GUIDv4::random());
+$processor = new ProcessingManager("users", $healthCheck, $logger, $logStream);
+$processor->initialize("handleItem", 55);
