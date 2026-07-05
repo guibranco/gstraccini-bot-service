0bfbf36f82fe035e6bd98b28cf975dfd0ae5fd3a
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2026-07-05 15:42:23 +0100

    chore: update dependencies in composer.lock
    Update "guibranco/pancake" from v1.7.20 to v1.7.23 and "nikic/php-parser"
    from v5.7.0 to v5.8.0 to incorporate the latest features and bug fixes.
    Also update "phpunit/phpunit" from 13.1.10 to 13.1.13 for
    development requirements. These changes ensure compatibility with
    the latest improvements and security patches provided by the
    respective packages.

--- Src/api/v1/swagger.php
+++ Src/api/v1/swagger.php
@@ -13,12 +13,12 @@ $pageTitle = 'GStraccini Bot API v' . htmlspecialchars($version) . ' - Swagger U
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= $pageTitle ?></title>
-  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.32.6/swagger-ui.css" integrity="sha384-9Q2fpS+xeS4ffJy6CagnwoUl+4ldAYhOs9pgZuEKxypVModhmZFzeMlvVsAjf7uT" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.32.8/swagger-ui.css" integrity="sha384-9Q2fpS+xeS4ffJy6CagnwoUl+4ldAYhOs9pgZuEKxypVModhmZFzeMlvVsAjf7uT" crossorigin="anonymous">
 </head>
 <body>
 <div id="swagger-ui"></div>
-<script src="https://unpkg.com/swagger-ui-dist@5.32.6/swagger-ui-bundle.js" integrity="sha384-EYdOaiRwn44zNjrw+Tfs06qYz9BGQVo2f4/pLY5i7VorbjnZNhdplAbTBk8FXHUJ" crossorigin="anonymous"></script>
-<script src="https://unpkg.com/swagger-ui-dist@5.32.6/swagger-ui-standalone-preset.js" integrity="sha384-49fpFaVrAWI/qdgl9Vv5E/4NXxRUiJX5vGuLws1NUpTWGtEqzWEx8gHTw2UTehFK" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/swagger-ui-dist@5.32.8/swagger-ui-bundle.js" integrity="sha384-IKpAWwsTL0pcw7/Amtnt2eXF4P1BK64WNuY2E/RG15SWLUW5HXzFuyqCSAr/DP8C" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/swagger-ui-dist@5.32.8/swagger-ui-standalone-preset.js" integrity="sha384-sm24U+dUFhSIgEfhSy6d7F66jTzh7YHwjwcdFANJ87OCxOWdQPERHk3xR2MtzMLa" crossorigin="anonymous"></script>
 <script>
   window.onload = function () {
     SwaggerUIBundle({
